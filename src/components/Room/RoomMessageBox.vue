bc862602b4fc5fa32c1dc3750382919c7cf7f276
PaoloTolentino <tolentinopaolomatthew23@gmail.com>
2024-08-27 23:54:31 +1000

    chore: migrate to pnpm and add Caddy support

--- src/components/Room/RoomMessageBox.vue
+++ src/components/Room/RoomMessageBox.vue
@@ -4,13 +4,13 @@
       v-model="message"
       @keyup.enter="sendMessage"
       type="text"
-      class="w-full p-2 focus:outline-none bg-chatbox-bg-light rounded-tl-lg rounded-bl-lg"
+      class="w-full p-2 rounded-tl-lg rounded-bl-lg focus:outline-none bg-chatbox-bg-light"
       placeholder="Enter Message"
     />
-    <div class="bg-chatbox-bg-light rounded-tr-lg rounded-br-lg">
+    <div class="rounded-tr-lg rounded-br-lg bg-chatbox-bg-light">
       <button
         @click="sendMessage"
-        class="rounded-full text-white p-3 focus:outline-none hover:bg-gray-600"
+        class="p-3 text-white rounded-full focus:outline-none hover:bg-gray-600"
       >
         <font-awesome-icon
           size="lg"
@@ -22,28 +22,27 @@
   </div>
 </template>
 
-<script setup >
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { ref, useContext } from "vue";
-import { useAuthentication } from "../../modules/authentication.js";
-import { useRooms } from "../../modules/room.js";
-import { useSocketEmit } from "../../modules/websocket.js";
+<script setup>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { ref } from 'vue'
+import { useAuthentication } from '../../modules/authentication.js'
+import { useRooms } from '../../modules/room.js'
+import { useSocketEmit } from '../../modules/websocket.js'
 
-const message = ref("");
-const context = useContext();
+const message = ref('')
 function sendMessage() {
-  if (message.value !== "") {
-    const { joinedRoom } = useRooms();
-    const { user } = useAuthentication();
-    useSocketEmit("chat message", {
+  if (message.value !== '') {
+    const { joinedRoom } = useRooms()
+    const { user } = useAuthentication()
+    useSocketEmit('chat message', {
       roomID: joinedRoom._id,
       message: {
         username: user.username,
-        content: message.value,
-      },
-    });
+        content: message.value
+      }
+    })
     // Reset the message once sent
-    message.value = "";
+    message.value = ''
   }
 }
 </script>
